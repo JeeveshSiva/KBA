402ad325d4b70f3d2350798679b2e904b3c7784a
“Anupatom” <anupatom498@gmail.com>
2023-05-16 00:33:45 +0530

    code

---
+++ Day2_javascript/main.js
@@ -0,0 +1,76 @@
+'use strict'
+//console.log("Node Basis")
+
+
+// let a=20
+// console.log(a)
+// const b=40
+// console.log(b)
+
+// let c=`Day2`
+// console.log(c)
+
+// let d=true
+// console.log(d)
+
+// let e //undefined
+// console.log(e)
+
+// let f=null//object
+// console.log(f)
+
+// console.log(e==f)
+//console.log(e===f)
+
+// const employee={
+//     Name:"Rahul",
+//     id:101,
+//     address:{l1:"Flat2c",
+//              l2:"Tvm"}
+// }
+// console.log(employee.address)
+
+// const employee={
+//                 EMP1:{Name:"Rahul",
+//                      id:101,
+//                      address:{l1:"Flat2c",
+//                               l2:"Tvm"}
+//                      },
+//                 EMP2:{Name:"Vinay",
+//                      id:101,
+//                      address:{l1:"Flat2d",
+//                               l2:"Tsr"}
+//                      }
+//              }
+//     console.log(employee.EMP1.address)
+
+
+// let arr1=new Array()
+// let arr2=[]
+// let arr3=[20,30,40]
+// console.log(arr3)
+// console.log(arr3[0])
+
+// let arr3=[20,30,40]
+// for(let i of arr3)
+// console.log(i)
+
+// function square(num){
+//     console.log("&&&&&&&",num*num)
+//     return num*num
+// }
+// let result=square(3)
+// console.log(result)
+
+// const square=(num) => {console.log("&&&&&&&",num*num)
+// return num*num}
+// let result=square(3)
+// console.log(result)
+
+
+const square=(num) => num*num
+// let result=square(3)
+// console.log(result)
+
+
+module.exports={square}
---
+++ Day2_javascript/main1.js
@@ -0,0 +1,68 @@
+// const ex=require('./main')
+// console.log(ex.square(100))
+
+//  const student1 = {
+//     name:'anil',
+//     age:34,
+//     details: function() {
+//         console.log(student1.name)
+//     }
+// }
+// student1.details()
+
+
+// const student2 = {
+//     name:'anna',
+//     age:39,
+//     details: function() {
+//         console.log(student1.name)
+//     }
+// }
+// student2.details()
+
+
+// function positive(numbers) {
+//     if(numbers > 0)
+//     {
+//         console.log(numbers)
+//        console.log('positive');
+//     }
+//     else
+//     {
+//         console.log(numbers)
+//         console.log('negative');  
+//     }
+// }
+// const test=(positive)
+// test(-33)
+
+
+// const numbers = 3
+// let p = new Promise((resolve,reject) => {
+//   if(numbers >0)
+//    {
+//        resolve('Positive')
+//    }
+//    else
+//    {
+//       reject('Negative')
+//   }
+// })
+// p.then((message) => {console.log('This is the message from then '+message)
+//                      console.log (numbers)} ).catch((message) =>
+// {console.log('This is the message from catch '+message)
+// console.log (numbers)
+// })
+
+// console.log('Start')
+// async function greet(){
+// let p =  await new Promise((resolve,reject) => {
+// setTimeout(()=> resolve('Hello'), 1000)
+  
+// })
+
+// let result = p
+// console.log(result)
+// }
+// greet()
+// console.log('End')
